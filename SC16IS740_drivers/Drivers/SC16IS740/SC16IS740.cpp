1073e331b58fab20ba6c11da40ef7bd8ba0cc0f1
JuanPBP03 <juanpablobecerra103@gmail.com>
2025-06-23 16:45:21 -0400

    started workspace and completed register definition macros

---
+++ SC16IS740_drivers/Drivers/SC16IS740/SC16IS740.cpp
@@ -0,0 +1,15 @@
+/*
+ * SC16IS740.cpp
+ *
+ *  Created on: Jun 23, 2025
+ *      Author: JuanP
+ */
+
+#ifndef SC16IS740_SC16IS740_CPP_
+#define SC16IS740_SC16IS740_CPP_
+
+
+
+
+
+#endif /* SC16IS740_SC16IS740_CPP_ */
---
+++ SC16IS740_drivers/Drivers/SC16IS740/SC16IS740.hpp
@@ -0,0 +1,15 @@
+/*
+ * SC16IS740.h
+ *
+ *  Created on: Jun 23, 2025
+ *      Author: JuanP
+ */
+
+#ifndef SC16IS740_SC16IS740_HPP_
+#define SC16IS740_SC16IS740_HPP_
+
+
+
+
+
+#endif /* SC16IS740_SC16IS740_HPP_ */
