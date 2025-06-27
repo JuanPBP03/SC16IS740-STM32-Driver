dfd4f0103131b9614f14034b20b7a83ea20a3d71
JuanPBP03 <juanpablobecerra103@gmail.com>
2025-06-27 14:06:24 -0400

    created function prototypes and config/handle structs

--- SC16IS740_drivers/Drivers/SC16IS740/SC16IS740.h
+++ SC16IS740_drivers/Drivers/SC16IS740/SC16IS740.h
@@ -13,18 +13,34 @@
  * 	Handle/Context and Config Structs
  */
 
-typedef struct
-{
-	uint16_t baudRate;
-	uint8_t
-}SC16IS740cfg_t;
+
 
 typedef struct
 {
+	uint16_t baudRate;
+	uint8_t parity;
+	uint8_t stopBits;
+	uint8_t wordLen;
+}SC16IS740cfg_t;
+
+
+typedef struct
+{
+	uint8_t addr;
 	SC16IS740cfg_t config;
-	void (*regWrite)(uint8_t regAddr, uint8_t regVal);
-	uint8_t (*regRead)(uint8_t regAddr);
+	void (*writeByte)(uint8_t regAddr, uint8_t regVal);
+	uint8_t (*readByte)(uint8_t regAddr);
 	uint8_t state;
 }SC16IS740handle_t;
+
+
+void IS740_init(SC16IS740handle_t *hIS740);
+
+void IS740_transmitByte(uint8_t addr, uint8_t txByte);
+
+uint8_t IS740_receiveByte(uint8_t addr);
+
+void IS740_writeReg(uint8_t regAddr, uint8_t regVal);
+uint8_t IS740_readReg(uint8_t regAddr, uint8_t regVal);
 
 #endif /* SC16IS740_SC16IS740_H_ */
